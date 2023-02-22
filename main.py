0063f5af68413784d2649940b0513c708b6809fe
maaz-zubair-99 <mzubair@stu.naperville203.org>
2023-02-21 22:16:51 -0600

    got it connecting to an xbox controller and made a simple driving thing

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from approxeng.input.selectbinder import ControllerResource
-
+from time import sleep
+import turtle
+acc = 1
+rev_acc = 3
+max_v = 200
+vel = 0
 while True:
     try:
         with ControllerResource() as joystick:
             print('Found a joystick and connected')
             while joystick.connected:
                 # Do stuff with your joystick here!
-                # ....
+                if joystick.r2 is not None and joystick.l2 is None:
+                    vel=min(vel+acc,max_v)
+                if joystick.l2 is not None and joystick.r2 is None:
+                    vel=max(vel-rev_acc,0)
+                turtle.forward(vel)
+                vel*=0.9
+                turtle.right(joystick['lx']*15)
                 # ....
         # Joystick disconnected...
         print('Connection to joystick lost')
